4430c27b1ce01c51abd4d582be78775bcbf981ae
Gregory Haskins <greg@greghaskins.com>
2014-03-31 10:16:52 +0530

    Rename app to wtfTimeIsThat

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('theretimeApp', ['ui.bootstrap']);
+angular.module('wtfTimeIsThatApp', ['ui.bootstrap']);
--- test/spec/controllers/main.spec.js
+++ test/spec/controllers/main.spec.js
@@ -3,7 +3,7 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('theretimeApp'));
+  beforeEach(module('wtfTimeIsThatApp'));
 
   var MainCtrl;
   var scope;
